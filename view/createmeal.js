cf1ab7c2833c7b15d6dc92239877b034d6240408
Kristian <kristian.risvaer@gmail.com>
2021-02-04 11:08:44 +0100

    04.02.2021

--- view/createmeal.js
+++ view/createmeal.js
@@ -8,15 +8,15 @@ function createMealView(mealChange) {
                     <td></td>
                     <td></td>
                     <td>Quantity</td>
-                    <td>Optional</td>
-                    <td></td>
+                    <td class="createMealCheckboxTD">Optional</td>
+                    <td class="createMealRemoveTD"></td>
                 </tr>
                 <tr>
                     <td>Meal Name</td>
-                    <td><input type="text" value="${model.createMeal.mealName}" onchange="model.createMeal.mealName = this.value"></td>
-                    <td>${mealChange ? '<button onclick="saveChangedMeal(model.savedMealsValues.index)">Save Changes</button>' : '<button onclick="saveMeal()">Save Meal</button>'}</td>
+                    <td><input class="createMealInputs" type="text" value="${model.createMeal.mealName}" onchange="model.createMeal.mealName = this.value"></td>
                     <td></td>
-                    <td></td>
+                    <td class="createMealCheckboxTD"></td>
+                    <td class="createMealRemoveTD"></td>
                 </tr>`;
                 let ingredients = model.createMeal.ingredients;
                 for (let i = 0; i < ingredients.length; i++) {
@@ -24,16 +24,16 @@ function createMealView(mealChange) {
                     <tr>
                         <td>Ingredient ${i+1}:</td>
                         <td>
-                            <input type="text" value="${ingredients[i].ingredient}" onchange="model.createMeal.ingredients[${i}].ingredient = this.value">
+                            <input class="createMealInputs" type="text" value="${ingredients[i].ingredient}" onchange="model.createMeal.ingredients[${i}].ingredient = this.value">
                         </td>
                         <td>
-                            <input type="text" value="${ingredients[i].quantity}" onchange="model.createMeal.ingredients[${i}].quantity = this.value">
+                            <input class="createMealInputs" type="text" value="${ingredients[i].quantity}" onchange="model.createMeal.ingredients[${i}].quantity = this.value">
                         </td>
-                        <td>
-                            <input id="createMealOptionalCheck${i}" type="checkbox" ${ingredients[i].optional ? 'checked' : ''} onclick="createMealCheckbox(this, ${i})">
+                        <td class="createMealCheckboxTD">
+                            <input id="createMealOptionalCheck${i}" class="createMealCheckboxes" type="checkbox" ${ingredients[i].optional ? 'checked' : ''} onclick="createMealCheckbox(this, ${i})">
                         </td>
-                        <td>
-                            <button onclick="deleteCreateMealIngredient(${mealChange+ ',' +i})">Remove</button>
+                        <td class="createMealRemoveTD">
+                            <button class="createMealRemoveButton" onclick="deleteCreateMealIngredient(${mealChange+ ',' +i})">Remove</button>
                         </td>
                     </tr>
                     `;
@@ -43,8 +43,9 @@ function createMealView(mealChange) {
                     <td></td>
                     <td>
                         <button onclick="addIngredientCreateMeal(${mealChange})">Add Ingredient &#43;</button>
+                        ${mealChange ? '<button onclick="saveChangedMeal(model.savedMealsValues.index)">Save Changes</button>' : '<button onclick="saveMeal()">Save Meal</button>'}
                     </td>
-                    <td></td>
+                    <td class="createMealCheckboxTD"></td>
                     <td></td>
                 </tr>
             </table>
@@ -52,7 +53,7 @@ function createMealView(mealChange) {
     
     <div id="createRecipeDiv" style="float: right; width: 40%; height: 90%;">
         <h1>Recipe:</h1>
-        <textarea type="text" style="width: 80%;" rows ="30" value="${model.createMeal.recipe}" onchange="model.createMeal.recipe = this.value"></textarea>
+        <textarea id="createMealTextarea" rows ="30" value="${model.createMeal.recipe}" onchange="model.createMeal.recipe = this.value"></textarea>
     </div>
     `;
     document.getElementById('app').innerHTML = html;
