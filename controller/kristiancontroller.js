8bab89cba6ab1b231ae85f91436c5674be5d75e2
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-02-04 10:22:39 +0100

    asdf

--- controller/kristiancontroller.js
+++ controller/kristiancontroller.js
@@ -200,9 +200,9 @@ function getFromLocalStorage() {
     }
 }
 
-window.onbeforeunload = function() {
-    saveToLocalStorage();
-}
-window.onload = function() {
-    getFromLocalStorage();
-}
+// window.onbeforeunload = function() {
+//     saveToLocalStorage();
+// }
+// window.onload = function() {
+//     getFromLocalStorage();
+// }
